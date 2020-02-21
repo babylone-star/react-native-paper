6e45f86c406c76efe7ed39e7eb04950afe38a4c1
James Strong <james.strong@babylonhealth.com>
2020-02-21 08:00:24 -0600

    More SVG Icons

--- src/components/SVGIcon/iconRegistry.js
+++ src/components/SVGIcon/iconRegistry.js
@@ -10,6 +10,12 @@ import Appointments from './appointments.svg';
 import Profile from './profile.svg';
 import Mood from './mood.svg';
 
+import ActivityLarge from './activity_large.svg';
+import BodyLarge from './body_large.svg';
+import BuildYourTwinLarge from './build_your_twin_large.svg';
+import MoodLarge from './mood_large.svg';
+import NutritionLarge from './nutrition_large.svg';
+
 const registry = {
   navClose: Close,
   help: Help,
@@ -22,6 +28,12 @@ const registry = {
   appointments: Appointments,
   profile: Profile,
   mood: Mood,
+
+  activityLarge: ActivityLarge,
+  bodyLarge: BodyLarge,
+  buildYourTwinLarge: BuildYourTwinLarge,
+  moodLarge: MoodLarge,
+  nutritionLarge: NutritionLarge,
 };
 
 export default registry;
